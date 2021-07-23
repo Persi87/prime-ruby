9bfe535a9a12d358ff43e88f069cce2754711588
Persi87 <simon_black87@hotmail.co.uk>
2021-07-23 22:35:29 +0100

    commit tree 35aa8fabfa0e4f8e02fe1fb51b23d6125024c2e6

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  i = 2
+    if number > 1                      # if number is greater than 1
+      check = (i...number - 1).to_a    # make an array from 2 to whatever the number is - 1 (we don't need to check the number itself)
+          check.none? do |num|         # run none? through the numbers in array 
+        number % num == 0              # if number % by element should give true 
+          end 
+      else
+          false                        
+    end  
+end
